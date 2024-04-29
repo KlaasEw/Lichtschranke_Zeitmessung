ad61fdc19c1a20577ca533911da2cd270770747d
KlaasEw <klaas.ewald@gmail.com>
2024-04-29 12:01:28 +0200

    Erweiterung um Testmodus

--- Lichtschranke_Zeitmessung/src/main.cpp
+++ Lichtschranke_Zeitmessung/src/main.cpp
@@ -11,17 +11,28 @@
 #define PIEZO_PIN 5
 //Pin für Reset
 #define RESET_PIN 7
+//Pin für Testmode
+#define Testmode_PIN 12
+#define Testmode_LED 13
 
 // Initialisierung des Display-Objekts
 TM1637Display display(CLK_PIN, DIO_PIN);
 
 // Variable, um den aktuellen Zustand des Timers zu speichern (gestartet oder gestoppt)
 bool timerRunning = false;
+
+// Variable, um den aktuellen Zustand der Lichtschranken zu speichern
 bool Start_State= HIGH;
 bool previous_Start_State = HIGH;
 bool END_State= HIGH;
 bool previous_END_State = HIGH;
+
+// Variable, um den aktuellen Zustand des Resettasters zu speichern
 bool Reset_State = HIGH;
+
+// Variable, um den aktuellen Zustand des Testmode Schalters zu speichern
+bool Testmode_State = HIGH;
+int Test_Response = 0;
 
 // Start- und Stop-Zeit
 unsigned long startTime = 0;
@@ -38,14 +49,19 @@ void setup() {
 
   // Konfiguration des Piezo-Pins
   pinMode(PIEZO_PIN, OUTPUT);
+  digitalWrite(PIEZO_PIN, LOW);
 
   // Konfiguration des Reset-Pins
   pinMode(RESET_PIN, INPUT_PULLUP);
 
+  // Konfiguration des Testmode-Pins und der Testmode LED
+  pinMode(Testmode_PIN, INPUT_PULLUP);
+  pinMode(Testmode_LED, OUTPUT);
+  digitalWrite(Testmode_LED, LOW);
+
   //Starten der Seriellen Schnittstelle
   Serial.begin(9600);
 }
-
 
 // Funktion zum Starten des Timers
 void startTimer() {
@@ -77,54 +93,88 @@ void loop() {
   Start_State = digitalRead(START_GATE_PIN);
   END_State = digitalRead(END_GATE_PIN);
   Reset_State = digitalRead(RESET_PIN);
+  Testmode_State = !digitalRead(Testmode_PIN);
 
-
-//Lichtschranke Start
-  if (Start_State == LOW && previous_Start_State == HIGH && timerRunning == LOW) {
-    startTimer();
-    previous_Start_State = LOW;
-    Serial.println("Start");
-    digitalWrite(PIEZO_PIN, HIGH);
-    delay(50);
-  }
-
-//Lichtschranke Start zurücksetzen
-  if (Start_State == HIGH && previous_Start_State == LOW) {
-    Serial.println("Start/Stop");
-    previous_Start_State = HIGH;
-    digitalWrite(PIEZO_PIN, LOW);
-    delay(50);
-  }
-
-//Lichtschranke Stop
-  if (END_State == LOW && previous_END_State == HIGH && timerRunning == HIGH) {
+  if (Testmode_State == 0 && Test_Response != 0) {
+    Test_Response = 0;
+    digitalWrite(Testmode_LED, LOW);
     stopTimer();
-    previous_END_State = LOW;
-    Serial.println("Stop");
-    digitalWrite(PIEZO_PIN, HIGH);
-    delay(50);
+    display.showNumberDecEx(0, 0b01000000, true);
   }
 
-//Lichtschranke Start zurücksetzen
-  if (END_State == HIGH && previous_END_State == LOW) {
-    Serial.println("Start/Stop");
-    previous_END_State = HIGH;
-    digitalWrite(PIEZO_PIN, LOW);
-    delay(50);
-  }
+  switch (Testmode_State){
+    case 0: // Normalmodus
+      //Lichtschranke Start
+      if (Start_State == LOW && previous_Start_State == HIGH && timerRunning == LOW) {
+        startTimer();
+        previous_Start_State = LOW;
+        Serial.println("Start");
+        digitalWrite(PIEZO_PIN, HIGH);
+        delay(50);
+      }
 
-//Timer auf 0 zurücksetzen
-  if(Reset_State == LOW) {
-    resetTimer();
-  }
+    //Lichtschranke Start zurücksetzen
+      if (Start_State == HIGH && previous_Start_State == LOW) {
+        Serial.println("Start/Stop");
+        previous_Start_State = HIGH;
+        digitalWrite(PIEZO_PIN, LOW);
+        delay(50);
+      }
 
-  // Überprüfen, ob der Timer läuft
-  if (timerRunning) {
-    // Aktuelle Zeit abrufen
-    unsigned long currentTime = millis();
+    //Lichtschranke Stop
+      if (END_State == LOW && previous_END_State == HIGH && timerRunning == HIGH) {
+        stopTimer();
+        previous_END_State = LOW;
+        Serial.println("Stop");
+        digitalWrite(PIEZO_PIN, HIGH);
+        delay(50);
+      }
 
-    // Anzeige der vergangenen Zeit
-    displayMillis(currentTime - startTime);
+    //Lichtschranke Start zurücksetzen
+      if (END_State == HIGH && previous_END_State == LOW) {
+        Serial.println("Start/Stop");
+        previous_END_State = HIGH;
+        digitalWrite(PIEZO_PIN, LOW);
+        delay(50);
+      }
+
+    //Timer auf 0 zurücksetzen
+      if(Reset_State == LOW) {
+        resetTimer();
+      }
+
+      // Überprüfen, ob der Timer läuft
+      if (timerRunning) {
+        // Aktuelle Zeit abrufen
+        unsigned long currentTime = millis();
+
+        // Anzeige der vergangenen Zeit
+        displayMillis(currentTime - startTime);
+      }
+    break;
+
+    case 1:
+      Serial.print("TestMode: ");
+      timerRunning = false;
+      digitalWrite(Testmode_LED, HIGH);
+      if (Start_State == HIGH) {
+        Test_Response = 1000;
+      } else {
+        Test_Response = 0;
+      }
+
+      if (END_State == HIGH) {
+        Test_Response = Test_Response + 1;
+      }
+
+      display.showNumberDecEx(Test_Response, 0b01000000, true);
+      Serial.println(Test_Response);
+
+    break;
+
+    default:
+      Serial.println("Error: TestmodeSwitch, false reading");
+    break;
   }
   
 }
