b204eb77ef0f132ca301297e898907782ebeda12
KlaasEw <klaas.ewald@gmail.com>
2024-04-29 11:57:42 +0200

    Erweiterung um eine Zweite Lichtschranke

--- Lichtschranke_Zeitmessung/src/main.cpp
+++ Lichtschranke_Zeitmessung/src/main.cpp
@@ -4,18 +4,24 @@
 // Pins für die 7-Segment-Anzeige
 #define CLK_PIN 2
 #define DIO_PIN 3
-// Pin für den Taster
-#define BUTTON_PIN 4
+// Pin für die Lichtschranken
+#define START_GATE_PIN 4
+#define END_GATE_PIN 6
 //Pin für Piezo
 #define PIEZO_PIN 5
+//Pin für Reset
+#define RESET_PIN 7
 
 // Initialisierung des Display-Objekts
 TM1637Display display(CLK_PIN, DIO_PIN);
 
 // Variable, um den aktuellen Zustand des Timers zu speichern (gestartet oder gestoppt)
 bool timerRunning = false;
-bool buttonState= HIGH;
-bool previousButtonState = HIGH;
+bool Start_State= HIGH;
+bool previous_Start_State = HIGH;
+bool END_State= HIGH;
+bool previous_END_State = HIGH;
+bool Reset_State = HIGH;
 
 // Start- und Stop-Zeit
 unsigned long startTime = 0;
@@ -26,15 +32,20 @@ void setup() {
   display.setBrightness(0x0a);  // Helligkeit einstellen (0x00 bis 0x0f)
   display.showNumberDecEx(0, 0b01000000, true);
 
-  // Konfiguration des Taster-Pins
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  // Konfiguration der Lichtschranken-Pins
+  pinMode(START_GATE_PIN, INPUT_PULLUP);
+  pinMode(END_GATE_PIN, INPUT_PULLUP);
 
   // Konfiguration des Piezo-Pins
   pinMode(PIEZO_PIN, OUTPUT);
 
+  // Konfiguration des Reset-Pins
+  pinMode(RESET_PIN, INPUT_PULLUP);
+
   //Starten der Seriellen Schnittstelle
   Serial.begin(9600);
 }
+
 
 // Funktion zum Starten des Timers
 void startTimer() {
@@ -55,34 +66,56 @@ void displayMillis(unsigned long milliseconds) {
   display.showNumberDecEx(milliseconds/10, 0b01000000, true);
 }
 
+// Funktion zum Zurücksetzen des Timers
+void resetTimer() {
+  stopTimer();
+  display.showNumberDecEx(0, 0b01000000, true);
+}
+
 void loop() {
   // Lesen des Tasterzustands
-  buttonState = digitalRead(BUTTON_PIN);
+  Start_State = digitalRead(START_GATE_PIN);
+  END_State = digitalRead(END_GATE_PIN);
+  Reset_State = digitalRead(RESET_PIN);
 
-//Taster Starten
-  if (buttonState == LOW && previousButtonState == HIGH && timerRunning == LOW) {
+
+//Lichtschranke Start
+  if (Start_State == LOW && previous_Start_State == HIGH && timerRunning == LOW) {
     startTimer();
-    previousButtonState = LOW;
+    previous_Start_State = LOW;
     Serial.println("Start");
     digitalWrite(PIEZO_PIN, HIGH);
     delay(50);
   }
 
-//Taster Stop
-  if (buttonState == LOW && previousButtonState == HIGH && timerRunning == HIGH) {
+//Lichtschranke Start zurücksetzen
+  if (Start_State == HIGH && previous_Start_State == LOW) {
+    Serial.println("Start/Stop");
+    previous_Start_State = HIGH;
+    digitalWrite(PIEZO_PIN, LOW);
+    delay(50);
+  }
+
+//Lichtschranke Stop
+  if (END_State == LOW && previous_END_State == HIGH && timerRunning == HIGH) {
     stopTimer();
-    previousButtonState = LOW;
+    previous_END_State = LOW;
     Serial.println("Stop");
     digitalWrite(PIEZO_PIN, HIGH);
     delay(50);
   }
 
-//Taster zurücksetzen
-  if (buttonState == HIGH && previousButtonState == LOW) {
+//Lichtschranke Start zurücksetzen
+  if (END_State == HIGH && previous_END_State == LOW) {
     Serial.println("Start/Stop");
-    previousButtonState = HIGH;
+    previous_END_State = HIGH;
     digitalWrite(PIEZO_PIN, LOW);
     delay(50);
+  }
+
+//Timer auf 0 zurücksetzen
+  if(Reset_State == LOW) {
+    resetTimer();
   }
 
   // Überprüfen, ob der Timer läuft
